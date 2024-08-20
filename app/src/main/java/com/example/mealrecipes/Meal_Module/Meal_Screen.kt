58b728fe14bd1d2c95e932524a783c6268d96bb0
monykappa <kimmony039@gmail.com>
2024-08-20 22:51:22 +0700

    feat:style home page

--- app/src/main/java/com/example/mealrecipes/Meal_Module/Meal_Screen.kt
+++ app/src/main/java/com/example/mealrecipes/Meal_Module/Meal_Screen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -24,14 +25,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
+import com.example.mealrecipes.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MealScreen(navController: NavHostController, viewModel: MealViewModel = viewModel()) {
     val categories by viewModel.categories.collectAsState(emptyList())
@@ -48,89 +52,113 @@ fun MealScreen(navController: NavHostController, viewModel: MealViewModel = view
     }
 
     Scaffold(
-        snackbarHost = { SnackbarHost(snackbarHostState) }
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo),
+                            contentDescription = "Brand Logo",
+                            modifier = Modifier.size(130.dp)
+                        )
+                    }
+                },
+                navigationIcon = {},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF35374B), // Set your custom color here
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        }
     ) { innerPadding ->
-        LazyColumn(modifier = Modifier.padding(innerPadding)) {
-            // Carousel of meal images and names
-            item {
-                Carousel(meals = areaMeals) { meal ->
-                    MealCarouselItem(
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFEFAF6))
+        ) {
+            LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                // Carousel of meal images and names
+                item {
+                    Carousel(meals = areaMeals) { meal ->
+                        MealCarouselItem(
+                            meal = meal,
+                            onClick = { selectedMeal ->
+                                navController.navigate("MealDetail/${selectedMeal.strMeal}")
+                            }
+                        )
+
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                }
+
+                // Category Buttons
+                item {
+                    LazyRow {
+                        items(categories) { category ->
+                            val isSelected = category.strCategory == selectedCategory.value
+                            Button(
+                                onClick = {
+                                    selectedCategory.value = category.strCategory
+                                    viewModel.clearMeals() // Clear current meals to provide immediate feedback
+                                    viewModel.fetchMeals(category = category.strCategory)
+                                },
+                                modifier = Modifier.padding(horizontal = 4.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF35374B),  // Set background color
+                                    contentColor = Color.White           // Set text color to white
+                                )
+                            ) {
+                                AsyncImage(
+                                    model = category.strCategoryThumb,
+                                    contentDescription = category.strCategory,
+                                    modifier = Modifier.size(40.dp)
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(text = category.strCategory)
+                            }
+                        }
+                    }
+                }
+
+
+                // Error message
+                error?.let {
+                    item {
+                        Text("Error: $it", color = MaterialTheme.colorScheme.error)
+                    }
+                }
+
+
+                // Meal list
+                items(meals) { meal ->
+                    MealItem(
                         meal = meal,
+                        isLoading = isLoadingMeals,
+                        viewModel = viewModel,
                         onClick = { selectedMeal ->
                             navController.navigate("MealDetail/${selectedMeal.strMeal}")
+                        },
+                        onFavoriteChanged = { isFavorite ->
+                            coroutineScope.launch {
+                                val message = if (isFavorite) {
+                                    "Added to favorites"
+                                } else {
+                                    "Removed from favorites"
+                                }
+                                snackbarHostState.showSnackbar(message)
+                            }
                         }
                     )
                 }
             }
-
-            // Category Buttons
-            item {
-                LazyRow {
-                    items(categories) { category ->
-                        val isSelected = category.strCategory == selectedCategory.value
-                        Button(
-                            onClick = {
-                                selectedCategory.value = category.strCategory
-                                viewModel.clearMeals() // Clear current meals to provide immediate feedback
-                                viewModel.fetchMeals(category = category.strCategory)
-                            },
-                            modifier = Modifier.padding(horizontal = 4.dp),
-                            colors = if (isSelected) {
-                                ButtonDefaults.buttonColors(
-                                    containerColor = MaterialTheme.colorScheme.primary,
-                                    contentColor = MaterialTheme.colorScheme.onPrimary
-                                )
-                            } else {
-                                ButtonDefaults.buttonColors(
-                                    containerColor = MaterialTheme.colorScheme.surface,
-                                    contentColor = MaterialTheme.colorScheme.onSurface
-                                )
-                            }
-                        ) {
-                            AsyncImage(
-                                model = category.strCategoryThumb,
-                                contentDescription = category.strCategory,
-                                modifier = Modifier.size(40.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = category.strCategory)
-                        }
-                    }
-                }
-            }
-
-            // Error message
-            error?.let {
-                item {
-                    Text("Error: $it", color = MaterialTheme.colorScheme.error)
-                }
-            }
-
-            // Meal list
-            items(meals) { meal ->
-                MealItem(
-                    meal = meal,
-                    isLoading = isLoadingMeals,
-                    viewModel = viewModel,
-                    onClick = { selectedMeal ->
-                        navController.navigate("MealDetail/${selectedMeal.strMeal}")
-                    },
-                    onFavoriteChanged = { isFavorite ->
-                        coroutineScope.launch {
-                            val message = if (isFavorite) {
-                                "Added to favorites"
-                            } else {
-                                "Removed from favorites"
-                            }
-                            snackbarHostState.showSnackbar(message)
-                        }
-                    }
-                )
-            }
         }
     }
 }
-
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -145,8 +173,14 @@ fun Carousel(meals: List<Meal>, itemContent: @Composable (Meal) -> Unit) {
         if (meals.isEmpty()) {
             CircularProgressIndicator()
         } else {
-            HorizontalPager(state = pagerState) { page ->
-                itemContent(meals[page])
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFFEFAE0))
+            ) {
+                HorizontalPager(state = pagerState) { page ->
+                    itemContent(meals[page])
+                }
             }
             DotsIndicator(
                 totalDots = meals.size,
@@ -158,41 +192,37 @@ fun Carousel(meals: List<Meal>, itemContent: @Composable (Meal) -> Unit) {
     }
 }
 
+
 @Composable
 fun DotsIndicator(
     totalDots: Int,
     selectedIndex: Int,
-    selectedColor: Color,
-    unSelectedColor: Color
+    selectedColor: Color,   // Added parameter for selected dot color
+    unSelectedColor: Color  // Added parameter for unselected dot color
 ) {
     LazyRow(
         modifier = Modifier
             .wrapContentWidth()
             .wrapContentHeight()
+            .padding(8.dp) // Add padding if needed
     ) {
         items(totalDots) { index ->
-            if (index == selectedIndex) {
-                Box(
-                    modifier = Modifier
-                        .size(5.dp)
-                        .clip(CircleShape)
-                        .background(selectedColor)
-                )
-            } else {
-                Box(
-                    modifier = Modifier
-                        .size(5.dp)
-                        .clip(CircleShape)
-                        .background(unSelectedColor)
-                )
-            }
+            Box(
+                modifier = Modifier
+                    .size(8.dp) // Size of the dots
+                    .clip(CircleShape)
+                    .background(if (index == selectedIndex) selectedColor else unSelectedColor)
+            )
 
             if (index != totalDots - 1) {
-                Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+                Spacer(modifier = Modifier.width(4.dp)) // Adjust spacing between dots
             }
         }
     }
 }
+
+
+
 
 @Composable
 fun MealCarouselItem(meal: Meal, onClick: (Meal) -> Unit) {
@@ -202,23 +232,42 @@ fun MealCarouselItem(meal: Meal, onClick: (Meal) -> Unit) {
             .fillMaxWidth()
             .clickable { onClick(meal) }
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFFEFAF6))
         ) {
-            AsyncImage(
-                model = meal.strMealThumb,
-                contentDescription = meal.strMeal,
-                modifier = Modifier.size(200.dp)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = meal.strMeal,
-                style = MaterialTheme.typography.titleMedium
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth().padding(16.dp)  // Ensures the content is centered and padded
+            ) {
+                AsyncImage(
+                    model = meal.strMealThumb,
+                    contentDescription = meal.strMeal,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16 / 20f)  // Adjust aspect ratio as needed
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()  // Ensures the background fills the entire width
+                        .background(Color(0xFF35374B))  // Semi-transparent dark background (alpha 50%)
+                        .padding(8.dp)  // Add some padding around the text
+                ) {
+                    Text(
+                        text = meal.strMeal,
+                        style = MaterialTheme.typography.titleMedium.copy(color = Color.White), // Text color black
+                        modifier = Modifier.align(Alignment.Center) // Center the text within the Box
+                    )
+                }
+            }
         }
     }
 }
+
+
 
 @Composable
 fun MealItem(
